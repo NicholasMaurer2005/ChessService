0ce1e4fb1e8da020c4599a41939d6962b30025fd
Salockin <salockin9@gmail.com>
2026-01-16 22:56:58 -0500

    bug fix

--- ChessService/Api/Controllers/SecureController.cs
+++ ChessService/Api/Controllers/SecureController.cs
@@ -10,11 +10,6 @@ namespace Backend.Api.Controllers
     public class SecureController : ControllerBase
     {
         //private members
-        protected string? _userId { get; }
-
-        public SecureController()
-        {
-            _userId = User.FindFirstValue("sub");
-        }
+        protected string UserId => User?.FindFirstValue("sub") ?? string.Empty;
     }
 }
